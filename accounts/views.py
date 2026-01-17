d8941733d1be91a7e3447eda0a2446813bb8dece
kevxpingu <kevdasah@gmail.com>
2026-01-17 14:48:48 +0000

    calculate and show customer and order informations

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+
+    total_orders = orders.count()
+    orders_pending = orders.filter(status="Pending").count()
+    orders_delivered = orders.filter(status="Delivered").count()
+    context = {
+        'customers': customers,
+        'orders': orders,
+        'total_orders': total_orders,
+        'orders_pending': orders_pending,
+        'orders_delivered': orders_delivered
+    }
+    return render(request, 'accounts/dashboard.html', context)
 
 def customers(request):
     return render(request, "accounts/customers.html")
 
 def products(request):
-    return render(request, "accounts/products.html")
+    products = Product.objects.all()
+    return render(request, "accounts/products.html", {'products': products})
