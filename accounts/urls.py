9d63c482b1fe424e590f0d450b52253bce8e069b
kevxpingu <kevdasah@gmail.com>
2026-01-17 17:59:31 +0000

    feat: add dynamic url pt1

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home" ),
-    path('customers/', views.customers, name="customers" ),
+    path('customers/<str:pk>', views.customers, name="customers" ),
     path('products/', views.products, name="products" )
 
 ]
